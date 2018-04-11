836ca14eb3ffd0287ebe054f017e22109f1c6256
rfliu2018 <mou0163@163.com>
2018-04-11 15:55:32 +0800

    1.cpp over

--- 18/1.cpp
+++ 18/1.cpp
@@ -5,6 +5,11 @@ typedef double ElemType;
 
 void del_min(SqList&l, ElemType&value)
 {
+	if (l.length <= 0)
+	{
+		cerr << "Error!\n";
+		exit(-1);
+	}
 	value = l[0];
 	int pos = 0;
 	for (int i = 1; i < l.length; i++)
@@ -17,22 +22,32 @@ void del_min(SqList&l, ElemType&value)
 	}
 	l[pos] = l[l.length - 1];
 	l.length--;
+
 }
 
 
 int main()
 {
-	SqList s(20);
+	double a[10] {18, 24, 53, 2, 4, 5, 6,  10, 6, 4};
+	SqList s(a, 10);
 	cout << s.length << endl;
 
-	for (int i = 0; i < s.length; i++)
-	{
-		s[i] = i * i;
-	}
 
 	for (int i = 0; i < s.length; i++)
 	{
 		cout << s[i] << "\t";
 	}
 	cout << endl;
+
+	double b;
+	del_min(s, b);
+
+	cout << s.length << endl;
+	cout << endl;
+	for (int i = 0; i < s.length; i++)
+	{
+		cout << s[i] << "\t";
+	}
+	cout << endl;
+
 }
